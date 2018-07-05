e7ebfaedb3e877ee2ae97e9b3e695e31924a47aa
Omar Cruz <ocruzvar@gmail.com>
2018-07-05 01:37:12 -0500

    Contextual menu

--- src/main/index.js
+++ src/main/index.js
@@ -1,5 +1,7 @@
 import { app, BrowserWindow } from 'electron' // eslint-disable-line
 
+require('electron-context-menu')(); // eslint-disable-line
+
 /**
  * Set `__static` path to static files in production
  * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-static-assets.html
